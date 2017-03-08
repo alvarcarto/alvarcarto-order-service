9d344dc9329ff535381cc56fbd76db4b534258a3
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-03-09 00:40:13 +0200

    Update to use new version of price util which uses cents for .value. Add safe limits for stripe charges

--- src/http/order-http.js
+++ src/http/order-http.js
@@ -2,11 +2,23 @@ const _ = require('lodash');
 const BPromise = require('bluebird');
 const { calculateCartPrice } = require('alvarcarto-price-util');
 const ex = require('../util/express');
+const logger = require('../util/logger')(__filename);
 const orderCore = require('../core/order-core');
 const stripe = require('../util/stripe');
 
+const SAFE_LIMIT_MIN = 20 * 100;
+const SAFE_LIMIT_MAX = 10000 * 100;
+
 const postOrder = ex.createJsonRoute((req) => {
   const price = calculateCartPrice(req.body.cart);
+  if (price.value < SAFE_LIMIT_MIN) {
+    logger.error(`Calculated price exceeded minimum safe limit: ${price}`);
+    ex.throwStatus(500, 'Internal price calculation failed');
+  }
+  if (price.value > SAFE_LIMIT_MAX) {
+    logger.error(`Calculated price exceeded maximum safe limit: ${price}`);
+    ex.throwStatus(500, 'Internal price calculation failed');
+  }
 
   return BPromise.resolve(stripe.charges.create({
     amount: price.value,
