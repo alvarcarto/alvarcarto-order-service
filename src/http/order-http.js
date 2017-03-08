4a30a69c9f5aba41080bf85744e5617d305bff17
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-03-08 23:51:29 +0200

    Calculate prices correctly

--- src/http/order-http.js
+++ src/http/order-http.js
@@ -1,15 +1,16 @@
 const _ = require('lodash');
 const BPromise = require('bluebird');
+const { calculateCartPrice } = require('alvarcarto-price-util');
 const ex = require('../util/express');
 const orderCore = require('../core/order-core');
 const stripe = require('../util/stripe');
 
 const postOrder = ex.createJsonRoute((req) => {
-  const price = { value: 1000, currency: 'eur' };
+  const price = calculateCartPrice(req.body.cart);
 
   return BPromise.resolve(stripe.charges.create({
     amount: price.value,
-    currency: price.currency,
+    currency: price.currency.toLowerCase(),
     source: req.body.stripeTokenResponse.id,
     receipt_email: req.body.email,
     description: `Charge for ${req.body.email}`,
