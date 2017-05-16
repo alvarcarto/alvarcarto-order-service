13b416f50b7b38fb65a303daa262e8cad8526d8b
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-05-16 14:44:07 +0300

    Urlencode query parameters when rendering poster

--- src/util/index.js
+++ src/util/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
+const qs = require('qs');
 const _ = require('lodash');
-const { oneLineTrim } = require('common-tags');
 const config = require('../config');
 
 function readFileSync(filePath) {
@@ -10,20 +10,24 @@ function readFileSync(filePath) {
 }
 
 function createPosterImageUrl(mapItem) {
-  return oneLineTrim`
-    ${config.RENDER_API_URL}/api/raster/render
-    ?swLat=${mapItem.mapBounds.southWest.lat}
-    &swLng=${mapItem.mapBounds.southWest.lng}
-    &neLat=${mapItem.mapBounds.northEast.lat}
-    &neLng=${mapItem.mapBounds.northEast.lng}
-    &style=${mapItem.mapStyle}
-    &size=${mapItem.size}
-    &orientation=${mapItem.orientation}
-    &labelsEnabled=${mapItem.labelsEnabled}
-    &labelHeader=${mapItem.labelHeader.toUpperCase()}
-    &labelSmallHeader=${mapItem.labelSmallHeader.toUpperCase()}
-    &labelText=${mapItem.labelText.toUpperCase()}
-  `;
+  const query = qs.stringify(createPosterUrlParameters(mapItem));
+  return `${config.RENDER_API_URL}/api/raster/render?${query}`;
+}
+
+function createPosterUrlParameters(mapItem) {
+  return {
+    swLat: mapItem.mapBounds.southWest.lat,
+    swLng: mapItem.mapBounds.southWest.lng,
+    neLat: mapItem.mapBounds.northEast.lat,
+    neLng: mapItem.mapBounds.northEast.lng,
+    style: mapItem.mapStyle,
+    size: mapItem.size,
+    orientation: mapItem.orientation,
+    labelsEnabled: mapItem.labelsEnabled,
+    labelHeader: mapItem.labelHeader.toUpperCase(),
+    labelSmallHeader: mapItem.labelSmallHeader.toUpperCase(),
+    labelText: mapItem.labelText.toUpperCase(),
+  };
 }
 
 function toLog(obj) {
