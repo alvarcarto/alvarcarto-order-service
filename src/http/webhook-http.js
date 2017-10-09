90df217715eb50838f5ba34bff509424dea6a3d8
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-10-09 17:26:13 +0300

    Remove alert from successful webhook calls

--- src/http/webhook-http.js
+++ src/http/webhook-http.js
@@ -3,7 +3,7 @@ const logger = require('../util/logger')(__filename);
 const webhookCore = require('../core/webhook-core');
 
 const postPrintmotor = ex.createJsonRoute((req) => {
-  logger.logEncrypted('info', 'alert-1h Webhook called:', req.body);
+  logger.logEncrypted('info', 'Webhook called:', req.body);
 
   const incomingHmac = req.headers['x-printmotor-hmac-sha256'];
   return webhookCore.savePrintmotorEvent(req.body, incomingHmac)
