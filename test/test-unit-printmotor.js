472a14eab2360a392b0d7327d2eb548fff41d263
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2019-09-24 10:30:25 +0300

    Add printmotor unit test torso for shipping calculations

---
+++ test/test-unit-printmotor.js
@@ -0,0 +1,11 @@
+/* eslint-env mocha */
+
+const printmotorCore = require('../src/core/printmotor-core');
+
+function test() {
+  describe('Printmotor unit tests', () => {
+
+  });
+}
+
+module.exports = test;
