4d805fdeffdc1a42c71a92dba0e36e0acb520c34
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2019-10-09 13:35:49 +0300

    Add script to clone prod db to local

---
+++ tools/prod-db-to-local.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+set -e
+
+docker-compose down
+docker volume rm postgres
+docker-compose up -d
+bash tools/pg-copy.sh $(heroku config:get DATABASE_URL -a alvarcarto-order-prod) $(grep DATABASE_URL .env | cut -d '=' -f2)
